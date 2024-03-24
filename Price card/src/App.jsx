59988cd6440d9e321fcf6b85510046346ceef2e2
kadangairaj <kadangairaj@gmail.com>
2024-03-24 23:08:27 +0530

    React

---
+++ Price card/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+  function a (){
+    setTimeout(() => {
+      
+    }, timeout);
+  }
+
+  return (
+    <>
+     <h1 className='H1'>Esakki</h1>
+     <button type='sumbit' onClick={a}></button>
+    </>
+  )
+}
+
+export default App
